74c0e215c48e9eb34e8a023ecd5140016a9f08a3
gtsofa <jlsnyule@yahoo.com>
2019-10-22 21:50:16 +0300

    break homepage to smaller components Finish styling our menu item components

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,42 +1,14 @@
 import React from 'react';
+// import MenuItem from '../../components/MenuItem/MenuItem';
+import Directory from '../../components/Directory/Directory';
+
 
 import './HomePage.styles.scss';
 
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">HATS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">JACKETS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">SNEAKERS</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">WOMEN</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">MEN</h1>
-                        <span className="subtitle">SHOP NOW</span>
-                    </div>
-                </div>
-            </div>
+            <Directory />
         </div>
     );
 }
