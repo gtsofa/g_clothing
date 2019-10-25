0c3acccaef157fef8472006ca9c5e3d23494ea46
gtsofa <jlsnyule@yahoo.com>
2019-10-25 23:43:38 +0300

    add Sign in component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/Shop';
+import SignInAndSignUpPage from './components/SignInAndSignUpPage/SignInAndSignUpPage';
 import Header from './components/Header/Header';
 
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
       
     </div>
